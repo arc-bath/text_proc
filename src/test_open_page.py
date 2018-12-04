9d61b5d3cb76aa0949a0104fc399365082aec1c3
James Grant <r.j.grant@bath.ac.uk>
2018-12-04 21:39:25 +0000

    Updated test file

--- src/test_open_page.py
+++ src/test_open_page.py
@@ -1,13 +1,11 @@
 import web_text as wt
 
 def test_licence():
-    link = "https://creativecommons.org/licenses/by/4.0/legalcode.txt"
-    filename = "../data/cc-by-4.0.txt"
+    link = "https://people.bath.ac.uk/rjg20/index.html"
+    filename = "../data/rjg20-index.html"
 
     with open(filename) as file:
-        expect = file.readlines()
+        expect = file.read().splitlines()
 
     link_str = wt.open_page(link)
     link_lines = link_str.splitlines()
-
-    
